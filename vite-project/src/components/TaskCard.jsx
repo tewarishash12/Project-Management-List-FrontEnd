23d159f35e1794cab7147b8e37316e09be1ad5b5
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-08 16:40:54 +0530

    Changed the  DashBoard component styling and updated it's look

--- vite-project/src/components/TaskCard.jsx
+++ vite-project/src/components/TaskCard.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const TaskCard = ({ task, darkMode }) => {
     return (
-        <div
-            className={`p-4 shadow-md rounded-md ${darkMode ? 'bg-gray-800 text-white' : 'bg-white text-gray-900'} sm:p-5 md:p-6 lg:p-8`}>
+        <div className={`p-4 shadow-md rounded-md ${darkMode ? 'bg-gray-800 text-white' : 'bg-white text-gray-900'} sm:p-5 md:p-6 lg:p-8`}>
             <h2 className="text-xl font-bold">{task.title}</h2>
             <p className="text-gray-400">{task.description}</p>
         </div>
